b72a6f205d6af809621ceb7c95f50eedd12357b3
samuel-coutinho <sfcrp2000@gmail.com>
2020-12-08 14:51:26 -0500

    making the contract

--- contracts/smartContract.sol
+++ contracts/smartContract.sol
@@ -1,36 +1,54 @@
 pragma solidity >=0.4.22 <0.8.0;
 
 contract SmartContracts {
-    struct product { // Struct
+    //test
+    bool column;
+    bool doors;
+    bool brakes;
+    bool cable;
+    bool battery_operating_permit;
+    uint [] certificate_of_conformity_list;
+
+    struct Product {
+
+        string client;
         string name;
         string component;
         bool verified;
-        uint64 quantity;
-        uint64 price
+        uint quantity;
+        uint64 price;
+        uint columnNumber;
+        uint certificate_of_conformity_unit;       
+    }  
+
+    Product new_prod;  
         
+    //function that will return true if the test passes    
+    function columnStatus(bool status) public {
+        column = status;
+    }
+    function doorsStatus(bool status) public {
+        doors = status;
+    }
+    function brakeStatus(bool status) public {
+        brakes = status;
+    }
+    function cableStatus(bool status) public {
+        cable = status;
+    }
+    function batteryStatus(bool battery_permit) public {
+        battery_operating_permit = battery_permit; 
     }
 
-
-    struct CertificatesAndPermits{
-        string certificates;
-        string permits;
-    }
-
-
-
-    struct authentificate { // Struct
-        string name;
-        bool verified;
-        uint64 quantity;
-        
-    }
+    function generateCertificate(bool column_status, bool doors_status, bool brakes_status, bool battery_operating_permit_status) public {
+        if (column_status == true && doors_status == true && brakes_status == true && battery_operating_permit_status == true)
+        {
+            certificate_of_conformity_list.push();
+        }
+    }  
 
 
-     struct test { // Struct
-        string name;
-        bool verified;
-        uint64 quantity;
-        
-    }
+
+
+
 }
-
