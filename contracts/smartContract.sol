49f8f575a305831c5b0a7e23ba5c728f74b45771
samuel-coutinho <sfcrp2000@gmail.com>
2020-12-08 23:30:59 -0500

    corrections

--- contracts/smartContract.sol
+++ contracts/smartContract.sol
@@ -26,67 +26,56 @@ contract SmartContract {
     }
     Product p;
 
-    function getNumberOfColumns(uint _numberOfColumns) public returns(uint _numberOfColumns){
+    function getNumberOfColumns(uint _numberOfColumns) public returns(uint numberOfColumns){
         return _numberOfColumns;
     }
-    function doorsStatus(bool _doorsStatus) public returns(bool _doorsStatus){
+    function getDoorsStatus(bool _doorsStatus) public returns(bool doorsStatus){
         return _doorsStatus;
     }
-    function brakeStatus(bool _brakeStatus) public returns(bool _brakeStatus){
+    function getBrakeStatus(bool _brakeStatus) public returns(bool brakeStatus){
         return _brakeStatus;
     }
-    function cableStatus(bool _cableStatus) public returns(bool _cableStatus){
+    function getCableStatus(bool _cableStatus) public returns(bool cableStatus){
         return _cableStatus;
     }
-    function batteryStatus(bool _batteryStatus) public returns(bool _batteryStatus){
+    function getBatteryStatus(bool _batteryStatus) public returns(bool batteryStatus){
         return _batteryStatus; 
     }
 
-    function generateCertificate(bool _isDoorsTestPass, bool _isBrakesTestPass, bool _isCableTestPass, bool _hasBatteryOperatingPermit) public {
+    function generateCertificate(uint i, bool _isDoorsTestPass, bool _isBrakesTestPass, bool _isCableTestPass, bool _hasBatteryOperatingPermit) public {
         if (_isDoorsTestPass == true && _isBrakesTestPass == true && _isCableTestPass == true && _hasBatteryOperatingPermit == true)
         {
-            self.hasColumnCertificateOfConformity = true;
+             columnList[i].hasColumnCertificateOfConformity = true;
         }
     } 
 
-    for (uint i = 0; i < numberOfColumns; i++)   {
-        columnList[i].isDoorsTestPass = doorsStatus(_doorsStatus);
-        columnList[i].isBrakesTestPass = brakeStatus(_brakeStatus);
-        columnList[i].isCableTestPass = cableStatus(_cableStatus);
-        columnList[i].hasBatteryOperatingPermit = batteryStatus(_batteryStatus);
-        generateCertificate(self.isDoorsTestPass, self.isBrakesTestPass, self.isCableTestPass, self.hasBatteryOperatingPermit)
+    function verifyColumn() public
+    {
+        for (uint i = 0; i < numberOfColumns; i++)   {
+            columnList[i].isDoorsTestPass = getDoorsStatus();
+            columnList[i].isBrakesTestPass = getBrakeStatus();
+            columnList[i].isCableTestPass = getCableStatus();
+            columnList[i].hasBatteryOperatingPermit = getBatteryStatus();
+            generateCertificate(i, columnList[i].isDoorsTestPass, columnList[i].isBrakesTestPass, columnList[i].isCableTestPass, columnList[i].hasBatteryOperatingPermit);
+        }
     }
 
     function isProductAproved() external returns(bool isAproved)
-    for (uint i = 0; i < numberOfColumns; i++)   {
-        p.isAproved = true;
-        if (column[i].hasColumnCertificateOfConformity != true)
-        {
-            p.isAproved = false;
+    {   
+        for (uint i = 0; i < numberOfColumns; i++)   {
+            p.isAproved = true;
+            if (columnList[i].hasColumnCertificateOfConformity != true)
+            {
+                p.isAproved = false;
+            }
+            return p.isAproved;
         }
-        return p.isAproved
-    }
-
-            
-    //function that will return true if the test passes    
-    // function columnStatus(bool status) public {
-    //     column = status;
-    // }    
-
-    //function generateCertificate(bool self.isDoorsTestPass, bool self.isBrakesTestPass, bool self.isCableTestPass, bool self.hasBatteryOperatingPermit) public {
-    function generateCertificate(bool self.isDoorsTestPass, bool self.isBrakesTestPass, bool self.isCableTestPass, bool self.hasBatteryOperatingPermit) public {
-        if (self.isDoorsTestPass == true && self.isBrakesTestPass == true && self.isCableTestPass == true && self.hasBatteryOperatingPermit == true)
-        {
-            hasColumnCertificateOfConformity = true;
-        }
-    }    
-
-
+    }       
+   
     // //function to get the value of certificte list
     // function getCertificate(uint initial) public view returns(uint) {
     //     return certificate_of_conformity_list[initial];
-    // }
-    
+    // }    
 
     // //function to get the number of certificates
     // function getNumberOfCertificate() public view returns(uint) {
