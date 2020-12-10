04f3f5eff2509b4b23e799504b505c9aa5fa41dd
“frimina” <friminazaddi@gmail.com>
2020-12-10 12:12:05 -0500

    form in vue.js is created and now we have to connect it with the contract

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -31,7 +31,24 @@
 </template>
 
 <script>
+import { mapGetters} from "vuex" 
+
+
 export default {
+  computed:{
+   ...mapGetters("drizzle", ["isDrizzleInitialized", "drizzleInstance"]),
+   ...mapGetters("contracts", ["getContractData", "contractInstances"]),
+   getDoorsStatus(){
+     !this.contractInstances ["smartContract"].synced;
+     let doors = [];
+     doors = this.getContractData({
+        contract: "smartContract", 
+        methode: "getDoorsStatus", 
+        toUtf8: false,
+        toAscii: false,
+     })
+   }
+  },
   name: 'HelloWorld',
   props: {
     msg: String
---
+++ frontend/src/components/smartContract.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="visa">
+      <br>
+      <br>
+    <h1>Input your project</h1>
+    <form>
+      <label for="first name"> Battery:</label>
+      <input type="number" required>
+      <br>
+      <label for="last name"> Columns:</label>
+      <input type="number" required>
+      <br>
+      <label for="country">Elevators:</label>
+      <input type="number"  required>
+      <br>
+      <label for="passport number">Floor:</label>
+      <input type="number"  required>
+
+     
+     <br><br>
+      <button>Submit</button>
+      <br>
+      
+    </form>
+</div>
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#visa {
+  margin: 20px auto;
+  max-width: 700px;
+}
+label{
+  display: block;
+  margin: 20px 0 10px;
+}
+input {
+  font-size: 30px;
+  border: 1px double rgb(102, 97, 96) ;
+  border-radius: 4px;
+}
+button {
+  font-size: 16px;
+ background: rgb(209, 65, 73);
+  padding: 0.4rem 1.3rem;
+  text-align: center;
+  border: none;
+  cursor: pointer;
+  border-radius: 4px;
+ margin: 10px;
+}
+span{
+  width: 30px;
+  float: right;
+  cursor: pointer;
+}
+span:hover{
+  color: brown;
+}
+.previous{
+  border: 1.5px solid;
+  padding:5px;
+  margin-bottom: 10px;
+}
+</style>
