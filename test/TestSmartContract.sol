f2347a5a62bcf4b5a1f39e6805cc2a55948bbd59
samuel-coutinho <sfcrp2000@gmail.com>
2020-12-10 11:19:42 -0500

    Function getBatteryStatus tested

--- test/TestSmartContract.sol
+++ test/TestSmartContract.sol
@@ -8,7 +8,14 @@ contract TestSmartContract {
   // The address of the SmartContract contract to be tested
   SmartContract smartcontract = SmartContract(DeployedAddresses.SmartContract());
 
+  // The expected values of the variables
   uint expectedNumberOfColumns = 2;
+  bool expectedDoorsStatus = true;
+  bool expectedBrakeStatus = true;
+  bool expectedCableStatus = true;
+  bool expectedBatteryStatus = true;
+  
+
 
   // Testing the getNumberOfColumns() function
   function testgetNumberOfColumns() public {     
@@ -21,5 +28,55 @@ contract TestSmartContract {
         "Function does not return the expected number of columns."
     );
   }
+
+  // Testing the getDoorsStatus() function
+  function testgetDoorsStatus() public {     
+
+    bool returnedDoorsStatus = smartcontract.getDoorsStatus(true);
+
+    Assert.equal(
+        returnedDoorsStatus,
+        expectedDoorsStatus,
+        "Function does not return the expected status of the doors."
+    );
+  }
+
+  // Testing the getBrakeStatus() function
+  function testgetBrakeStatus() public {     
+
+    bool returnedBrakeStatus = smartcontract.getBrakeStatus(true);
+
+    Assert.equal(
+        returnedBrakeStatus,
+        expectedDoorsStatus,
+        "Function does not return the expected status of the brakes."
+    );
+  }
+
+  // Testing the getCableStatus() function
+  function testgetCableStatus() public {     
+
+    bool returnedCableStatus = smartcontract.getCableStatus(true);
+
+    Assert.equal(
+        returnedCableStatus,
+        expectedCableStatus,
+        "Function does not return the expected status of the cables."
+    );
+  }
+
+  // Testing the getBatteryStatus() function
+  function testgetBatteryStatus() public {     
+
+    bool returnedBatteryStatus = smartcontract.getBatteryStatus(true);
+
+    Assert.equal(
+        returnedBatteryStatus,
+        expectedBatteryStatus,
+        "Function does not return the expected status of the battery."
+    );
+  }
+
+
 
 } 
