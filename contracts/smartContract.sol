7450e6f38b04004da3653d9eaa051182e27761c7
vnga-dao <dao.vnga@gmail.com>
2020-12-11 15:26:39 -0500

    restore commit before pull master

--- contracts/smartContract.sol
+++ contracts/smartContract.sol
@@ -1,83 +1,103 @@
 pragma solidity >=0.4.22 <0.8.0;
 
 contract SmartContract {
-
-    uint numberOfColumns;
+    uint256 numberOfColumns;
     bool doorsStatus;
-    bool brakeStatus;  
-    bool cableStatus; 
-    bool batteryStatus;    
+    bool brakeStatus;
+    bool cableStatus;
+    bool batteryStatus;
 
     struct Column {
-        bool isDoorsTestPass;   
+        bool isDoorsTestPass;
         bool isBrakesTestPass;
         bool isCableTestPass;
         bool hasBatteryOperatingPermit;
-        bool hasColumnCertificateOfConformity;                
-    } 
+        bool hasColumnCertificateOfConformity;
+    }
 
     Column[] public columnList;
-    
+
     struct Product {
         string client;
         string name;
         string component;
         bool isAproved;
-        uint quantity;
+        uint256 quantity;
         uint64 price;
-        uint columnNumber;            
+        uint256 columnNumber;
     }
-    
+
     Product p;
 
-    function getNumberOfColumns(uint _numberOfColumns) public pure returns(uint){
+    function getNumberOfColumns(uint256 _numberOfColumns)
+        public
+        pure
+        returns (uint256)
+    {
         return _numberOfColumns;
     }
-    function getDoorsStatus(bool _doorsStatus) public pure returns(bool){
+
+    function getDoorsStatus(bool _doorsStatus) public pure returns (bool) {
         return _doorsStatus;
     }
-    function getBrakeStatus(bool _brakeStatus) public pure returns(bool){
+
+    function getBrakeStatus(bool _brakeStatus) public pure returns (bool) {
         return _brakeStatus;
     }
-    function getCableStatus(bool _cableStatus) public pure returns(bool){
+
+    function getCableStatus(bool _cableStatus) public pure returns (bool) {
         return _cableStatus;
     }
-    function getBatteryStatus(bool _batteryStatus) public pure returns(bool){
-        return _batteryStatus; 
-    }
-    function generateCertificate(uint i, bool _isDoorsTestPass, bool _isBrakesTestPass, bool _isCableTestPass, bool _hasBatteryOperatingPermit) public {
-        if (_isDoorsTestPass == true && _isBrakesTestPass == true && _isCableTestPass == true && _hasBatteryOperatingPermit == true)
-        {
-             columnList[i].hasColumnCertificateOfConformity = true;
-        }
-    } 
 
-    function verifyColumn() public
-    {   
-        for (uint i = 0; i < numberOfColumns; i++)   {
+    function getBatteryStatus(bool _batteryStatus) public pure returns (bool) {
+        return _batteryStatus;
+    }
+
+    function generateCertificate(
+        uint256 i,
+        bool _isDoorsTestPass,
+        bool _isBrakesTestPass,
+        bool _isCableTestPass,
+        bool _hasBatteryOperatingPermit
+    ) public {
+        if (
+            _isDoorsTestPass == true &&
+            _isBrakesTestPass == true &&
+            _isCableTestPass == true &&
+            _hasBatteryOperatingPermit == true
+        ) {
+            columnList[i].hasColumnCertificateOfConformity = true;
+        }
+    }
+
+    function verifyColumn() public {
+        for (uint256 i = 0; i < numberOfColumns; i++) {
             columnList[i].isDoorsTestPass = getDoorsStatus(doorsStatus);
             columnList[i].isBrakesTestPass = getBrakeStatus(brakeStatus);
             columnList[i].isCableTestPass = getCableStatus(cableStatus);
-            columnList[i].hasBatteryOperatingPermit = getBatteryStatus(batteryStatus);
-            generateCertificate(i, columnList[i].isDoorsTestPass, columnList[i].isBrakesTestPass, columnList[i].isCableTestPass, columnList[i].hasBatteryOperatingPermit);
+            columnList[i].hasBatteryOperatingPermit = getBatteryStatus(
+                batteryStatus
+            );
+            generateCertificate(
+                i,
+                columnList[i].isDoorsTestPass,
+                columnList[i].isBrakesTestPass,
+                columnList[i].isCableTestPass,
+                columnList[i].hasBatteryOperatingPermit
+            );
         }
     }
-    function isProductAproved(uint _numberOfColumns) public returns(bool)
-    {   
-        for (uint i = 0; i < _numberOfColumns; i++)   {
+
+    function isProductAproved(uint256 _numberOfColumns) public returns (bool) {
+        for (uint256 i = 0; i < _numberOfColumns; i++) {
             p.isAproved = true;
-            if (columnList[i].hasColumnCertificateOfConformity != true)
-            {
+            if (columnList[i].hasColumnCertificateOfConformity != true) {
                 p.isAproved = false;
-            }            
+            }
         }
         return p.isAproved;
-    }      
-   
-   
-//https://medium.com/better-programming/how-to-test-ethereum-smart-contracts-35abc8fa199d
-//https://medium.com/@yangnana11/solidity-function-types-4ad4e5de6d56
-    
+    }
+
+    //https://medium.com/better-programming/how-to-test-ethereum-smart-contracts-35abc8fa199d
+    //https://medium.com/@yangnana11/solidity-function-types-4ad4e5de6d56
 }
-
-
